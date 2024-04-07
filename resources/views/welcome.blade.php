ecf9398b4a3ec1481037f7ea8b56e1c648117a1f
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-07 18:52:39 -0300

    adiciona conteudo intuitivo para usuario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,13 @@
 @section('title', 'Bem vindo ao FreeIdeia')
 
 @section('content')
-    
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Hello there</h1>
+        <p class="py-6">Seja muito bem vindo ao meu projeto chamado de: FreeIdeia, seu intuito é apenas divulgar suas opnioes/ideias com todo o publico! sem restrições, banimentos ou censuras, sinta-se livre para expor o que quer!</p>
+        <p>Faça <a href={{ route('users.create', ['id'=>1]) }} class="link link-secondary">Registre-se </a> para publicar sua Idea</p>
+      </div>
+    </div>
+  </div>    
 @endsection

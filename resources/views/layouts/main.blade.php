fefbc018376d6c1174f6c1e89631f6302b854f5f
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-07 15:00:28 -0300

    adiciona estilização inicial usando o daisyUI

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,11 +5,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="preconnect" href="https://fonts.bunny.net">
+  <html data-theme="night"></html>
   <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
   <title>@yield('title')</title>
+  <link href="https://cdn.jsdelivr.net/npm/daisyui@4.10.1/dist/full.min.css" rel="stylesheet" type="text/css" />
+  <script src="https://cdn.tailwindcss.com" defer></script>
   @yield('head')
 </head>
 <body>
+  @include('layouts.navbar')
   <main>
     @yield('content')
   </main>

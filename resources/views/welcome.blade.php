a6daa428fa8cc15c154417274fd6034aeda35f86
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-07 15:00:52 -0300

    implementa a extensão default do arquivo main.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
+@section('title', 'Bem vindo ao FreeIdeia')
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-    </head>
-    <body>
-        <div>
-            <h1>HELLO WORLD</h1>
-            @foreach ($posts as $post)
-                {{$post->title}}
-            @endforeach
-        </div>
-    </body>
-</html>
+@section('content')
+    
+@endsection

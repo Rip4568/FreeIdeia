488c789b14fb6a5aef1094e43fc0eecc41d510f1
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-07 20:22:30 -0300

    adiciona mensagens de erros para cada campo e persistencia de valor ao fazer requisição

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,22 +1,36 @@
 @extends('layouts.main')
 
 @section('content')
-<form action="">
+<form action="{{ route('users.store') }}" method="POST">
+  @csrf
   <div class="artboard-demo p-4 gap-2 min-h-screen">
+    @error('email')
+      <div class="text-red-500">{{ $message }}</div>
+    @enderror
     <label class="input input-bordered flex items-center gap-2">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-      <input type="text" class="grow" placeholder="Email" />
+      <input type="text" class="grow" name="email" value="{{ old('email') }}" placeholder="Email" />
+    </label>
+    @error('name')
+      <div class="text-red-500">{{ $message }}</div>
+    @enderror
+    <label class="input input-bordered flex items-center gap-2">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+      <input type="text" class="grow" name="name" value="{{ old('name') }}" placeholder="Nome Completo" />
+    </label>
+    @error('password')
+      <div class="text-red-500">{{ $message }}</div>
+    @enderror
+    <label class="input input-bordered flex items-center gap-2">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clip-rule="evenodd" /></svg>
+      <input type="password" class="grow" name="password" value="{{ old('password') }}" placeholder="Sua senha"/>
     </label>
     <label class="input input-bordered flex items-center gap-2">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clip-rule="evenodd" /></svg>
-      <input type="password" class="grow" placeholder="Sua senha"/>
+      <input type="password" class="grow" name="password_confirmation" value="{{ old('password_confirmation') }}" placeholder="Confirme sua senha"/>
     </label>
-    <label class="input input-bordered flex items-center gap-2">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clip-rule="evenodd" /></svg>
-      <input type="password" class="grow" placeholder="Confirme sua senha"/>
-    </label>
-    <button class="btn btn-secondary w-60 rounded-full" type="submit">Login</button>
-    <p>Já tem uma conta ? <a>Entre</a></p>
+    <button class="btn btn-secondary w-60 rounded-full" type="submit">Registrar</button>
+    <p>Já tem uma conta ? <a href='{{ route('users.showLogin') }}' class="link link-primary">Entre</a></p>
   </div>
 </form>
 @endsection

dd2bd2f7e671291dd5f1bbf88870d3707ffe909e
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-08 21:18:41 -0300

    corrigi redirecionamento

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -7,13 +7,13 @@
       <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
         <li><a>About</a></li>
         @guest
-        <li><a href={{ route('users.create') }}>Login</a></li>
+        <li><a href={{ route('users.showLogin') }}>Login</a></li>
         @endguest
         @auth
-        <li>
+        <li class="mx-auto w-full">
           <form action="{{ route('logout') }}" method="post">
             @csrf
-            <a class="btn btn-outline btn-error btn-sm ">Logout</a>
+            <button type="submit" class="btn btn-outline btn-error btn-sm w-auto">Logout</button>
           </form>
         </li>
         @endauth

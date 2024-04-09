0185a7aab59f23e9787232cbbe21555f2d151d10
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-09 11:53:22 -0300

    adiciona campo campo nulavbel para content

--- database/migrations/2024_04_07_025225_create_post_table.php
+++ database/migrations/2024_04_07_025225_create_post_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('content');
+            $table->text('content')->nullable();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();

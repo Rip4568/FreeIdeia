581d242c9a43baf8d8546e3d538e6c2264da19c8
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-10 15:19:21 -0300

    melhora mensagem de boas vindas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,15 @@
 <div class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Hello there</h1>
+        <h1 class="text-5xl font-bold">Bem vindo</h1>
         <p class="py-6">Seja muito bem vindo ao meu projeto chamado de: FreeIdeia, seu intuito é apenas divulgar suas opnioes/ideias com todo o publico! sem restrições, banimentos ou censuras, sinta-se livre para expor o que quer!</p>
         @guest()
-        <p><a href='{{ route('users.create') }}' class="link link-success">Registre-se</a> para publicar sua Idea</p>
+          <p><a href='{{ route('users.create') }}' class="link link-success">Registre-se</a> para publicar sua Idea</p>
         @endguest
       </div>
     </div>
 </div>
+
 
 <div class="stats shadow w-full flex flex-wrap justify-center items-center" >
   <div class="stat">
@@ -21,7 +22,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
     </div>
     <div class="stat-title">Posts Acessados</div>
-    <div class="stat-value">16</div>
+    <div class="stat-value">{{ $posts_clicked_quantity }}</div>
   </div>
   
   <div class="stat">
@@ -39,10 +40,11 @@
     <div class="stat-title">Posts Criados</div>
     <div class="stat-value">{{ $posts_quantity }}</div>
   </div>
-  
 </div>
 
+<h1 class="text-center text-5xl">Veja alguns exemplos dos posts mais recentes</h1>
 <section class="flex gap-6 w-full justify-center mx-6 mt-6 mb-6">
+  
   @foreach ($posts as $post)
   <div class="card w-96 bg-neutral shadow-xl">
     <div class="card-body">

1d89efb62cdac01216516abc0cdb21f25176afd5
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-10 15:18:33 -0300

    implementa logica por meio de um middleware de incrementação de do campo clicked para cada vez que um post for clicado

---
+++ app/Http/Middleware/IncrementPostClicked.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Post;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IncrementPostClicked
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        //increment o campo clicked do post
+        $post = Post::find($request->id);
+        $post->clicked = $post->clicked + 1;
+        $post->save();
+        return $next($request);
+    }
+}

8cadd4e60574e72ad9b62984e7bfb058e0426a38
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-11 00:11:42 -0300

    corrrigi logica da navbar caso o usuario esteja logado

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,13 +5,14 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </div>
       <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li>
-          <a class="btn btn-outline btn-acciant btn-sm">About</a>
-        </li>
         @guest
-        <li><a href={{ route('users.showLogin') }}>Login</a></li>
+        <li><a class="btn btn-outline btn-primary btn-sm" href={{ route('users.showLogin') }}>Login</a></li>
+        <li><a class="btn btn-outline btn-secondary btn-sm" href={{ route('users.create') }}>Signup</a></li>
         @endguest
         @auth
+        <li>
+          <a class="btn btn-outline btn-acciant btn-sm">Perfil</a>
+        </li>
         <li>
           <a class="btn btn-outline btn-success btn-sm" href="{{ route('posts.create') }}">New Post</a>
         </li>

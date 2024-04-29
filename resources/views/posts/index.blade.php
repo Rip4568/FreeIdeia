d907dfe8d55e761cbab484c1d4cde5e0a141c857
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-29 20:02:40 -0300

    criação de pagina para listagem de todos os posts adicionada

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,7 @@
     <div class="card w-96 bg-neutral shadow-xl">
       <div class="card-body">
         <h2 class="card-title">{{$post->title}}</h2>
-        <i class="card-title">Por: {{$post->user->name}}</i>
+        <i class="card-title">Por: {{$post->user->name}} <button class="btn btn-primary btn-sm">Follow</button> </i>
         <div class="card-actions justify-end">
           <a class="btn btn-secondary" href="{{ route('posts.show', ['post'=>$post]) }}">Leia o conteudo</a>
         </div>
@@ -16,19 +16,6 @@
   @endforeach
 </section>
 
-{{ $posts->links() }}
-{{-- <div class="btn-group mx-auto my-6">
-  @if ($posts->onFirstPage())
-      <button class="btn btn-disabled">Previous</button>
-  @else
-      <a href="{{ $posts->previousPageUrl() }}" class="btn btn-outline">Previous</a>
-  @endif
-
-  @if ($posts->hasMorePages())
-      <a href="{{ $posts->nextPageUrl() }}" class="btn btn-outline">Next</a>
-  @else
-      <button class="btn btn-disabled">Next</button>
-  @endif
-</div> --}}
+{{ $posts->appends(['search' => request()->input('search')])->links() }}
 
 @endsection

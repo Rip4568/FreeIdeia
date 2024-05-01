b6ed96f6355e0b080f8ff23bd52168e5358f6354
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-30 21:28:37 -0300

    logica inicial para vericair se o usario é ou não ja um seguidor de deteminado usuario

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,18 @@
     <div class="card w-96 bg-neutral shadow-xl">
       <div class="card-body">
         <h2 class="card-title">{{$post->title}}</h2>
-        <i class="card-title">Por: {{$post->user->name}} <button class="btn btn-primary btn-sm">Follow</button> </i>
+        <i class="card-title">Por: {{$post->user->name}}
+          @if (Auth::user()->following->contains($user->id))
+          <form action="{{ route('unfollow', $user->id) }}" method="POST">
+              @csrf
+              <button type="submit">Deixar de seguir</button>
+          </form>
+      @else
+          <form action="{{ route('follow', $user->id) }}" method="POST">
+              @csrf
+              <button type="submit">Seguir</button>
+          </form>
+      @endif</i>
         <div class="card-actions justify-end">
           <a class="btn btn-secondary" href="{{ route('posts.show', ['post'=>$post]) }}">Leia o conteudo</a>
         </div>

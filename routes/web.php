977ceb4e349f665862847d6e47ca2d9612e97afe
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-10 15:58:33 -0300

    traduz a mensagem explicativa da existencia do arquivo web.php

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Aqui é onde você pode registrar rotas web para seu aplicativo. Essas
+| rotas são carregadas pelo RouteServiceProvider e todas elas serão
+| atribuídas ao grupo de middleware "web". Faça algo incrível!
 |
 */
 

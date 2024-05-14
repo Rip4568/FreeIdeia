fa42e77fa4a485dc1d990c211fe79d6059218616
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-14 16:23:07 -0300

    implementa função de atualização do usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -56,7 +57,7 @@ class UserController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
         return view('auth.signup');
     }
@@ -77,6 +78,8 @@ class UserController extends Controller
             'email' => $validatedData['email'],
             'password' => bcrypt($validatedData['password'])
         ]);
+
+        //Auth::login($user);//utilizar futuramente, facilitar a autenticação do usuário
 
 
         return redirect()->route('users.showLogin')->with('success', 'Conta criada com sucesso. Faça o login para acessar.');
@@ -103,7 +106,20 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'nullable|required|string|max:255',
+            'email' => 'nullable|required|email|unique:users,email,' . $user->id,
+            'password' => 'nullable|string|min:8|confirmed',
+        ]);
+
+        if ($request->filled('password')) {
+            $validatedData['password'] = Hash::make($validatedData['password']);
+        }
+
+        $user->update($validatedData);
+
+        return redirect()->route('users.index')
+            ->with('success', 'User updated successfully');
     }
 
     /**
@@ -111,6 +127,9 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return redirect()->route('posts.index')
+            ->with('success', 'User deleted successfully');
     }
 }

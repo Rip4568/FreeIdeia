9e50a053e48f7d8d45d34158ef7ecb64c3e9c4e5
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-17 16:14:40 -0300

    *

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Notification;
+use App\Models\User;
+use Database\Factories\NotificationFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        /* User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('test'),
+        ]); */
+        $users = User::all();
+        foreach ($users as $user) {
+            Notification::factory()->count(2)->create([
+                'user_id' => $user->id,
+            ]);
+        }
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([

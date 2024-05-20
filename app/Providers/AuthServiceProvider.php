6c01ad30e0295394afaaa5ea5df437026ac5dba2
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-20 11:59:05 -0300

    criação de portão (Gate) para uso futuro

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,9 +4,11 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 
+use App\Models\Post;
 use App\Models\User;
-use Illuminate\Auth\Access\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use LegacyTests\Browser\PostPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -16,7 +18,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        //Post::class => PostPol
     ];
 
     /**
@@ -24,6 +26,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        
+        Gate::define('admin', function (User $user) {
+            return $user->email === 'admin@admin.com';
+        });
     }
 }

8c40164277be8ce6a8bcfffa04c396428bdcfb41
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-22 15:51:32 -0300

    adiciona e implementa logica do UserRepository

---
+++ app/Repositories/UserRespository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+
+class UserRespository
+{
+  public function all()
+  {
+    return User::all();
+  }
+
+  public function findOne(string $id)
+  {
+    return User::find($id);
+  }
+  public function create(array $data)
+  {
+    return User::create($data);
+  }
+  public function update(string $id, array $data) {
+    $user = User::find($id);
+    $user->update($data);
+    return $user;
+  }
+
+  public function delete($id) {
+    $user = User::find($id);
+    return $user->delete();
+  }
+}

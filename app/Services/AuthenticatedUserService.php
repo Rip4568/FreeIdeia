e2aea609633008a37287286d7927a4ae74a67bb3
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-22 14:28:24 -0300

    NÃO USAR

--- app/Services/AuthenticatedUserService.php
+++ app/Services/AuthenticatedUserService.php
@@ -17,16 +17,22 @@ class AuthenticatedUserService
    */
   public static function getAuthenticatedUser(): User
   {
-    $cacheKey = 'authenticated_user';
-    $user = Cache::get($cacheKey);
+    $cacheKey = 'authenticated_user_' . session()->getId();
+    $user = Cache::get($cacheKey); //pegue o user da cache
 
     if (!$user) {
+      /* se não encontra, pegue o user da session */
       $user = Session::get('authenticated_user');
     }
 
     if (!$user) {
+      /* se tabem não encontra, pegue o usario autenticado pelo Auth fazendo
+       * uma busca no BD */
       $user = Auth::user();
+      /* apos a busca aramazene o usuario na cache
+      por 10 minutos para melhorar o desempenho (se estiver configurado) */
       Cache::put($cacheKey, $user, now()->addMinutes(10)); // Armazena o usuário na cache por 10 minutos
+
       Session::put('authenticated_user', $user); // Armazena o usuário na sessão
     }
 

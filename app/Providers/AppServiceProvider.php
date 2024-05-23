cf004ab665d8275dcb4a008f724ecb58ac412e53
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-23 12:16:18 -0300

    adiciona o PostRepository e PostService no AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Repositories\PostRepository;
 use App\Repositories\UserRespository;
+use App\Services\PostService\PostService;
 use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
@@ -18,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
         });
         $this->app->singleton(UserService::class, function ($app) {
             return new UserService($app->make(UserRespository::class));
+        });
+
+        $this->app->singleton(PostRepository::class, function ($app) {
+            return new PostRepository();
+        });
+        $this->app->singleton(PostService::class, function ($app) {
+            return new PostService($app->make(PostRepository::class));
         });
     }
 

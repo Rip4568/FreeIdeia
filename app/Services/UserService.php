fc6daafc4e4460a403471c792f70e8179d88040a
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-23 12:20:00 -0300

    implementa funcionalidades faltas para o UserService

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 namespace App\Services;
 
 use App\Models\User;
@@ -7,8 +8,33 @@ use App\Repositories\UserRespository;
 class UserService
 {
   private UserRespository $userRepository;
-  public function __construct(UserRespository $userRepository) {
+  public function __construct(UserRespository $userRepository)
+  {
     $this->userRepository = $userRepository;
   }
 
+  public function create(array $data)
+  {
+    return $this->userRepository->create($data);
+  }
+
+  public function update(string $id, array $data)
+  {
+    return $this->userRepository->update($id, $data);
+  }
+
+  public function delete(string $id)
+  {
+    return $this->userRepository->delete($id);
+  }
+
+  public function find(string $id)
+  {
+    return $this->userRepository->find($id);
+  }
+
+  public function all()
+  {
+    return $this->userRepository->all();
+  }
 }

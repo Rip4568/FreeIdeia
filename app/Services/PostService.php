866e61a280cf212ad93d93756a8d39cbab9aee52
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-23 12:19:42 -0300

    implementa o PostSErvice

---
+++ app/Services/PostService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services\PostService;
+
+use App\Repositories\PostRepository;
+
+class PostService
+{
+  private PostRepository $postRespository;
+  
+  public function __construct(PostRepository $postRespository)
+  {
+    $this->postRespository = $postRespository;
+  }
+
+  public function create(array $data)
+  {
+    return $this->postRespository->create($data);
+  }
+
+  public function update(string $id, array $data)
+  {
+    return $this->postRespository->update($id, $data);
+  }
+
+  public function delete(string $id)
+  {
+    return $this->postRespository->delete($id);
+  }
+
+  public function find(string $id)
+  {
+    return $this->postRespository->find($id);
+  }
+}

819d715740df1cff5e368dd02957f4612125f627
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-23 12:09:18 -0300

    importação e adição do Pusher e Laravel Echo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
   <meta charset="UTF-8">
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="preconnect" href="https://fonts.bunny.net">
@@ -19,5 +20,6 @@
   </main>
 
   @include('layouts.footer')
+  {{-- <script src="{{ asset('resources/js/app.js') }}"></script> --}}
 </body>
 </html>

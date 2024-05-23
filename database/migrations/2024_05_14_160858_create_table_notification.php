a4fea94afe21f40ef50b52291cfd8cd028e47102
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-23 12:13:06 -0300

    aumenta tamanho maximo do title

--- database/migrations/2024_05_14_160858_create_table_notification.php
+++ database/migrations/2024_05_14_160858_create_table_notification.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreign('user_id')->references('id')->on('users');
             $table->enum('type', ['success', 'error', 'danger', 'primary', 'secondary'])->default('primary');
             $table->boolean('is_read')->default(false);
-            $table->string('title', 18);
+            $table->string('title', 32);
             $table->text('message')->nullable();
             $table->timestamps();
         });

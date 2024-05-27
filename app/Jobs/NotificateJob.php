b3d3c30b6ae0f93fd8ce5cacd34e0932644d863a
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-27 17:15:43 -0300

    implementa Laravel Reverb & Broadcasting

--- app/Jobs/NotificateJob.php
+++ app/Jobs/NotificateJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -15,7 +16,7 @@ class NotificateJob implements ShouldQueue
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct(User $user)
     {
         //
     }

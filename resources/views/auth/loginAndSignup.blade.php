8ba88002716f43c854eb7fbe6b4072eefec5c70e
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-05-28 14:33:14 -0300

    Broken: implementa Swap do DaisyUI para facilitar entre login e singup

---
+++ resources/views/auth/loginAndSignup.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('content')
+<label class="swap swap-flip text-9xl">
+  
+  <!-- this hidden checkbox controls the state -->
+  <input type="checkbox" />
+  
+  <div class="swap-on">😈
+    
+  </div>
+  <div class="swap-off">😇
+    
+  </div>
+</label>
+@endsection
